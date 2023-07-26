e20a8ad7e36c96f831cd1c51ab8d6b13ea9234b2
Mecit Mert Bişgin <98218585+mecitmert@users.noreply.github.com>
2023-07-26 13:01:02 +0300

    Add files via upload

---
+++ E_Bussiness_Command_67/Database_67.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Bussiness_Command
+{
+    public class Database
+    {
+      public  double version;
+       public String name;
+        public Database(double version , string name) 
+        {
+            this.version = version; 
+            this.name = name;   
+        }   
+    }
+}
---
+++ E_Bussiness_Command_67/Update_67.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Bussiness_Command
+{
+    internal class Update : IDataBase
+    {
+        private const int end_versiyon = 22;
+        Database database;
+        double temp;
+        public void Execute()
+        {
+            Console.WriteLine("Guncelleme Kontrol ediliyor...");
+            if (database.version == end_versiyon)
+                Console.WriteLine("Zaten en guncel durumda");
+            else
+            {
+                Console.WriteLine("Guncelleme Mevcut");
+                Console.WriteLine("Guncelleniyor...");
+                temp = database.version;
+                database.version = end_versiyon;
+                Console.WriteLine("Basariyla Guncelledendi : versiyon--" + database.version);
+            }
+        }
+
+        public void Repeat()
+        {
+            Execute();
+        }
+
+        public void Revoke()
+        {
+            Console.WriteLine("Islem Iptal talebi:");
+            Console.WriteLine("Islem Geri Aliniyor...");
+            database.version = temp;
+
+        }
+
+        public void setDatabase(Database database)
+        {
+            this.database = database;  
+        }
+
+        public void show()
+        {
+           Console.WriteLine(database.name + "version -  " + database.version);
+        }
+    }
+}
